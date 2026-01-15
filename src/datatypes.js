bf6d1e3d6db78bbfbbeeb5169d15565c1ffd8e57
mercy-5120 <mgashau18@gmail.com>
2026-01-15 10:04:03 +0300

    Showed the different data types

---
+++ src/datatypes.js
@@ -0,0 +1,19 @@
+//DATA TYPES IN JS
+//1.String - Represents text
+let email = "mgashau18@gmail.com"
+//2.Number - Represents integers and floating-point numbers
+let score = 49.789
+//3.Boolean -  Represents binary values, true or false, on or off
+let isLoggedIn = false
+//4.Object - Represents complex data structures.
+let user = {name: "Mercy", age: 19}
+//5.Array/List - Represents a list of values
+let fruits = ["apple", "banana", 90, [1,2,3]]
+//6.Undefined - Represents a variable that has been declared but not assigned a value
+let x;
+//7. Null - Represents the intentional absence of any object value
+let age = null
+//8. Symbol - Represents a unique identifier
+let sym = Symbol("id");
+
+//JavaScrript is a dynamically typed language, meaning you don't have to declare the type of variable explicitly. The type is determined at runtime based on the assigned  value
